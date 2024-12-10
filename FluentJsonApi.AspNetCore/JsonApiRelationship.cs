07ea1887e4937976d77cfb0ee9713fd1cf5dab2f
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2024-12-10 16:35:28 -0500

    Make JsonApiLinks property nullable in JsonApiRelationship class

--- FluentJsonApi.AspNetCore/JsonApiRelationship.cs
+++ FluentJsonApi.AspNetCore/JsonApiRelationship.cs
@@ -5,5 +5,5 @@ namespace FluentJsonApi.AspNetCore;
 public class JsonApiRelationship
 {
     public string Type { get; set; } = string.Empty;
-    public JsonApiLinks Links { get; set; } = new();
+    public JsonApiLinks? Links { get; set; }
 }
